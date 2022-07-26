f09bc1f1cfe0dfd762d5c806d9aedb7552e2e126
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2022-07-26 21:50:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,3 @@ def rss(username: str, tag: str):
             item.getparent().remove(item)
 
     return Response(content=etree.tostring(root, encoding="UTF-8"), media_type="text/xml")
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000)
